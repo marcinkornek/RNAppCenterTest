1407544b72a65b2d78b09695abca207e0359e9c5
Marcin Kornek <marcin.kornek@netguru.pl>
2018-11-27 11:15:37 +0100

    Add pre-build script to populate google-service.json file

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -7,3 +7,16 @@ printf "%s\n\n" $ENV_WHITELIST
 set | egrep -e $ENV_WHITELIST | egrep -v "^_" | egrep -v "WHITELIST" > .env
 printf "\n.env created with contents:\n"
 cat .env
+
+# pastes content of google-service.json file from ENV into blank google-services.json file
+GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/android/app/google-services.json
+
+if [ -e "$GOOGLE_JSON_FILE" ]
+then
+    echo "Updating Google Json"
+    echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
+    sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
+
+    echo "File content:"
+    cat $GOOGLE_JSON_FILE
+fi
